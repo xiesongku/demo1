c65256095a7b793ceb5996e79fb62bcb761b6017
yangxioayu19910928 <417535738@qq.com>
2019-10-25 11:55:58 +0800

    test

--- demo_1902/src/main/java/com/mr/demo_1902/controller/Test.java
+++ demo_1902/src/main/java/com/mr/demo_1902/controller/Test.java
@@ -3,6 +3,7 @@ package com.mr.demo_1902.controller;
 public class Test {
     public static void main(String[] args) {
         System.out.println("2019-10-25");
+        System.out.println(123123);
         System.out.println("hhhhhhhh");
     }
 }
